482621c5ff8e26bf4ac8f33e4ffb535f87d74519
Sascha Soerries <32453878+Sorayal@users.noreply.github.com>
2021-04-22 23:17:37 +0200

    Minor refactoring MIB_TCP_STATE

--- DatabasedTCPIPLoggingTool/WindowsAPI/MIB_TCP_STATE.cs
+++ DatabasedTCPIPLoggingTool/WindowsAPI/MIB_TCP_STATE.cs
@@ -3,16 +3,16 @@
     public enum MIB_TCP_STATE
     {
         Closed = 1,
-        Listen = 2,
-        SynSent = 3,
-        SynRcvd = 4,
-        Established = 5,
-        FinWait1 = 6,
-        FinWait2 = 7,
-        CloseWait = 8,
-        Closing = 9,
-        LastAck = 10,
-        TimeWait = 11,
-        DeleteTcb = 12
+        Listen,
+        SynSent,
+        SynRcvd,
+        Established,
+        FinWait1,
+        FinWait2,
+        CloseWait,
+        Closing,
+        LastAck,
+        TimeWait,
+        DeleteTcb
     }
 }
