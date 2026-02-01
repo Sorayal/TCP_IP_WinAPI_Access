92a71a42b80900ee7c3c0a1094e135f78759919a
sorayal <sorayal@my.mail.de>
2026-02-01 01:36:01 +0100

    Adding entity for data processing

---
+++ DatabasedTCPIPLoggingTool/Entities/NetworkConnection.cs
@@ -0,0 +1,16 @@
+﻿namespace TCPIPLoggingWinAPI.Entities;
+
+public class NetworkConnection
+{
+    public string Protocol { get; set; } // TCP oder UDP
+    public string LocalAddress { get; set; }
+    public int LocalPort { get; set; }
+    public string RemoteAddress { get; set; }
+    public int RemotePort { get; set; }
+    public string State { get; set; }
+    public int ProcessId { get; set; }
+    public string ProcessName { get; set; }
+    public string ProcessPath { get; set; }
+    public string Direction { get; set; } // in / out / loopback
+    public DateTime Timestamp { get; set; }
+}
